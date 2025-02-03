f39451077efd9b1f030f00d6cb2b317171c022fb
405262_Scipioni-Gabriel <gabriel.scipioni21@gmail.com>
2025-02-03 11:28:42 -0300

    A

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Segura?",
+    "Muy Segura??",
+    "Quisiste decir que si?",
+    "Es el otro boton...",
+    "Eso significa si?",
+    "Gay el que toca el boton rojo",
+    "uhhh loco...",
+    "pero presiona el boton siii",
+    "dejo de preguntar...",
+    "mentira, pone que si! ❤️"
 ];
 
 let messageIndex = 0;
